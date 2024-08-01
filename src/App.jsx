9d2719fd6fa72652199123891b1a1895d9292db4
Raju Kumar <rajuk.ug22.cs@nitp.ac.in>
2024-08-01 12:08:34 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -31,8 +31,8 @@ const copyPasswordToClipboard=useCallback(()=>{
 
   return (
     <>
-     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800 '>
-     <h1 className='text-white text-center my-3'>Password Generator</h1>
+     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-red-500 bg-gray-800 '>
+     <h1 className='text-white text-center my-3'>Password Generator Program</h1>
       <div className='flex shadow rounded-lg overflow-hidden mb-4"'>
         <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='Password' readOnly ref={passwordRef}/>
         <button onClick={copyPasswordToClipboard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
